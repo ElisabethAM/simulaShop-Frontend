a269f12005e8cc6a4f54a46cb98dfab4ef6b8427
kamisato03 <angmejia0324@gmail.com>
2024-07-30 01:57:25 -0600

    fix:configuraciones finales del proyecto

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
 
-createApp(App).mount('#app')
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import router from "./routes/index.js";
+import {api} from './boot/axios.js';
+
+//pinia
+import { createPinia } from "pinia";
+const pinia = createPinia();
+
+import App from "./App.vue";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App);
+app.config.globalProperties.$api = api;
+app.use(vuetify);
+app.use(pinia);
+app.use(router);
+app.mount("#app");
