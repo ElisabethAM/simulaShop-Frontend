68601ad3110ac547df83242ada79761ecd2072d9
kamisato03 <angmejia0324@gmail.com>
2024-08-04 21:42:41 -0600

    crud de productos

--- src/stores/product_store.js
+++ src/stores/product_store.js
@@ -7,7 +7,31 @@ export const useProductStore = defineStore("product", () => {
   const shopStore = useShopStore();
   const products = ref(null);
   const product = ref(null);
-  
+
+  const addProduct = async(producto)=>{
+    try {
+        console.log(producto);
+        const res = await axios({
+          url: `http://localhost:5000/api/stores/${producto.storeId}/products`,
+          method: "POST",
+          data: {
+            name: producto.name,
+            image: producto.image,
+            category: producto.category,
+            salePrice: producto.salePrice,
+            purchasePrice: producto.purchasePrice,
+            selectedForCycle: producto.selectedForCycle,
+            availableUnits: producto.availableUnits,
+            demandMin: producto.demandMin,
+            demandMax: producto.demandMax,
+          },
+        });
+        product.value = res.data.product;
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
   const getProducts = async () => {
     try {
       const res = await axios({
@@ -20,9 +44,54 @@ export const useProductStore = defineStore("product", () => {
     }
   };
 
+  const updateProduct = async (producto) => {
+    try {
+      console.log(producto);
+      const res = await axios({
+        url: `http://localhost:5000/api/stores/${shopStore.shop._id}/products/${producto._id}`,
+        method: "PUT",
+        data: {
+          name: producto.name,
+          image: producto.image,
+          category: producto.category,
+          salePrice: producto.salePrice,
+          purchasePrice: producto.purchasePrice,
+          selectedForCycle: producto.selectedForCycle,
+          availableUnits: producto.availableUnits,
+          demandMin: producto.demandMin,
+          demandMax: producto.demandMax,
+          isDeleted: producto.isDeleted,
+        },
+      });
+      product.value = res.data.product;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addUnitsProduct = async (producto, quantity) => {
+    try {
+      console.log(quantity);
+      const res = await axios({
+        url: `http://localhost:5000/api/stores/${shopStore.shop._id}/products/${producto._id}/add-units`,
+        method: "PUT",
+        data: {
+          additionalUnits: parseInt(quantity, 10),
+        },
+      });
+      product.value = res.data.product;
+      shopStore.shop = res.data.store;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return {
     products,
     getProducts,
+    updateProduct,
+    addUnitsProduct,
+    addProduct,
     product,
   };
 });
